dbe9027d3447a442736227a5f9cdc8d092d0fe78
ParamShukla007 <shuklaparam09022004@gmail.com>
2025-02-08 22:17:53 +0530

    added attandance

---
+++ backend/src/models/attendance.models.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose"; 
+
+const attendanceSchema = new mongoose.Schema({
+    officer: { 
+        type: Schema.Types.ObjectId, 
+        ref: "User", 
+        required: true 
+    },
+    status:{
+        type:String,
+        enum: ["present","absent"],
+        required: [true, "Type of status is required"]
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    },
+    in_time:{
+        type:String,
+        required: [true, "Time is required"],
+        trim: true
+    },
+    out_time:{
+        type:String,
+        required: [true, "Time is required"],
+        trim: true
+    }
+})
+
+export const Attendance = mongoose.model('Attendance',attendanceSchema);
+
