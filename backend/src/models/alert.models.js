6a28f49b5cfa0ec23c41b9a60551986668a0d4af
ParamShukla007 <shuklaparam09022004@gmail.com>
2025-02-08 18:05:42 +0530

    added alert backend

---
+++ backend/src/models/alert.models.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const alertSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    imageUrl:{
+        type:String
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+const Alert = mongoose.model('Alert',alertSchema);
+
+export default Alert
